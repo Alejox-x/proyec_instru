250ef9417e0435bf9734ba0f7f60937ea5afc9d5
alejox-x <alejo33rp@gmail.com>
2024-09-25 13:19:42 -0500

    acordion

--- instrumentos_24.py
+++ instrumentos_24.py
@@ -20,7 +20,10 @@ class Flauta(InstrumentoMusical):
 class Bateria(InstrumentoMusical):
     def tocar(self):
         return f"{self.nombre} está tocando un ritmo de batería."
-
+    
+class Acordion(InstrumentoMusical):
+    def tocar(self):
+        return f"{self.nombre} está tocando ballenato"
 def gestionar_instrumentos(instrumentos):
     for instrumento in instrumentos:
         print(instrumento.tocar())
@@ -31,7 +34,8 @@ def mostrar_menu():
     print("2. Ver y tocar Piano")
     print("3. Ver y tocar Flauta")
     print("4. Ver y tocar Batería")
-    print("5. Salir")
+    print("5. ver y tocar Acordion")
+    print("6. salir")
 
 def main():
     # Crear instancias de instrumentos musicales
@@ -39,19 +43,21 @@ def main():
     piano = Piano("Piano de Cola")
     flauta = Flauta("Flauta Traversera")
     bateria = Bateria("Batería Acústica")
+    acordion= Acordion("ritmo de acordion")
 
     instrumentos = {
         1: guitarra,
         2: piano,
         3: flauta,
-        4: bateria
+        4: bateria,
+        5: acordion
     }
 
     while True:
         mostrar_menu()
         try:
             opcion = int(input("Seleccione una opción: "))
-            if opcion == 5:
+            if opcion == 6:
                 print("Saliendo del sistema.")
                 break
             elif opcion in instrumentos:
